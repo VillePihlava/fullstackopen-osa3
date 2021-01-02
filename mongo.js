1da1ef528e0918c7996f82ff4be1ebd583f1cfff
VillePihlava <ville.pihlava@aalto.fi>
2021-01-03 01:28:20 +0200

    Osa 3 valmis

--- mongo.js
+++ mongo.js
@@ -1,9 +1,8 @@
-const { response } = require('express')
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,33 +12,33 @@ const url = `mongodb+srv://fullstackopen:${password}@cluster0.cj64b.mongodb.net/
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length == 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  person
+    .save()
+    .then(() => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`)
+      mongoose.connection.close()
     })
-    person
-        .save()
-        .then(response => {
-            console.log(`added ${person.name} number ${person.number} to phonebook`)
-            mongoose.connection.close()
-        })
-} else if (process.argv.length == 3) {
-    Person
-        .find({})
-        .then(persons => {
-            console.log('phonebook:')
-            persons.forEach(person => {
-                console.log(`${person.name} ${person.number}`)
-            })
-            mongoose.connection.close()
-        })
+} else if (process.argv.length === 3) {
+  Person
+    .find({})
+    .then(persons => {
+      console.log('phonebook:')
+      persons.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
 } else {
-    process.exit(1)
+  process.exit(1)
 }
